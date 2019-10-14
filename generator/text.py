f96b83f91e96f1b3769eb2ea0d7663bdc0c2be5c
Lennart Suwe <lennart.suwe@lfda.de>
2019-10-14 15:44:05 +0200

    feat: list commits test

--- generator/text.py
+++ generator/text.py
@@ -4,4 +4,31 @@ def test(repo_path, commit):
     repo = git.Repo(repo_path)
     master = repo.heads.master
     log = master.log()
-    print(log[commit])
+
+    for item in log:
+
+        message = item.message
+        message = message.strip('commit: ')
+        message = message.split('\n')
+
+        commit = {}
+
+        try:
+            commit['type'] = message[0][:message[0].index(': ')]
+            commit['description'] = message[0][message[0].index(': ')+2:]
+        except ValueError:
+            continue
+
+        if commit['type'] not in ['feat', 'fix', 'refactor']:
+            continue
+
+        if len(message) > 1:
+            commit['body'] = message[1]
+        else:
+            commit['body'] = None
+        if len(message) > 2:
+            commit['footer'] = message[2]
+        else:
+            commit['footer'] = None
+
+        print(commit)
