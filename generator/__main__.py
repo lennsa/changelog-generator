c3b051b9ecce0b74e71d301f338e3de9c04258cf
Lennart Suwe <lennart.suwe@lfda.de>
2019-10-16 13:35:53 +0200

    near finish

--- generator/__main__.py
+++ generator/__main__.py
@@ -1,5 +1,5 @@
 import click
-from text import get_commits
+from text import Repo
 
 @click.group()
 def generator():
@@ -7,9 +7,23 @@ def generator():
 
 @generator.command()
 @click.argument('repo_path')
-@click.option('--commit', default=-1, help='number of greetings')
-def generate(repo_path, commit):
-    commits = get_commits(repo_path)
+def generate(repo_path):
+    repo = Repo(repo_path)
+    # commits = repo.get_commits()
+    # for commit in commits:
+    #     print(commit['description'])
+    # tags = repo.get_tags()
+    # for tag in tags:
+    #     print(tag)
+    text = repo.generate_changelog()
+    # print(text)
+
+@generator.command()
+@click.argument('repo_path')
+@click.option('--commit', default=-1, help='number of commit (latest =^ 0)')
+def getcommit(repo_path, commit):
+    repo = Repo(repo_path)
+    commits = repo.get_commits()
     print(commits[commit])
 
 if __name__ == '__main__':
--- generator/text.py
+++ generator/text.py
@@ -1,20 +1,37 @@
 import git
 
-def get_commits(repo_path):
 
-    repo = git.Repo(repo_path)
+def pop_list(pop_list):
+    for item in pop_list:
+        yield item
 
-    commits = list(repo.iter_commits("master"))
 
-    commits_list = []
+class Repo():
 
-    for commit in commits:
+    def __init__(self, repo_path):
+        self.repo = git.Repo(repo_path)
 
+    def get_commits(self):
+
+        commits = list(self.repo.iter_commits("master"))
+
+        commits_list = []
+
+        for commit in commits:
+
+            commits_list.append(self.commit_dict(commit))
+        
+        return commits_list
+
+    def commit_dict(self, commit):
+        
         message = commit.message
 
         message = message.split('\n')
 
         commit_dict = {}
+
+        commit_dict['binsha'] = commit.binsha
 
         commit_dict['type'] = None
         commit_dict['scope'] = None
@@ -51,7 +68,65 @@ def get_commits(repo_path):
                 commit_dict['body'] += line + '\n'
             elif pos == 2:
                 commit_dict['footer'] += line + '\n'
+                
+        return commit_dict
 
-        commits_list.append(commit_dict)
-    
-    return commits_list
+    def get_tags(self):
+        
+        tags = self.repo.tags
+
+        tags_list = []
+
+        for tag in tags:
+
+            tag_dict = {}
+
+            tag_dict['name'] = tag.name
+
+            if tag.object.type == 'commit':
+                commit = tag.object
+            else:
+                commit = tag.object.object
+            tag_dict['commit'] = commit.binsha
+
+            tags_list.append(tag_dict)
+
+        return tags_list
+
+    def generate_changelog(self):
+
+        commits = self.get_commits()
+        tags = self.get_tags()
+
+        print('commits:',len(commits))
+        print('tags:', len(tags))
+
+        commits = pop_list(commits)
+        tags.reverse()
+        tags = pop_list(tags)
+
+        releace = []
+        i = 0
+        
+        for tag in tags:
+            i += 1
+            for commit in commits:
+                releace.append(commit)
+                if commit['binsha'] == tag['commit']:
+                    print(tag['name'], 'commits:', len(releace))
+                    releace = []
+                    i -= 1
+                    break
+        
+        print('rest commits:', len(releace))
+        print('rest tags:', i)
+
+        # while tags:
+        #     tag = tags.pop()
+        #     while commits:
+        #         commit = commits.pop()
+        #         releace.append(commit)
+        #         if commit['binsha'] == tag['commit']:
+        #             print(tag['name'], 'commits:', len(releace), 'rest commits:', len(commits))
+        #             releace = []
+        #             break
