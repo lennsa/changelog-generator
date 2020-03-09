e87ce38573bded0e6e15954a0ad3c58de2601a19
Lennart Suwe <lennart.suwe@lfda.de>
2020-03-09 15:37:06 +0100

    fix: error message when CHANGELOG.md was not found

--- changelog/changelog_generator.py
+++ changelog/changelog_generator.py
@@ -1,4 +1,6 @@
 import click
+import os
+import git
 
 from changelog.gitrepo import Repo
 
@@ -9,7 +11,7 @@ def generator():
     pass
 
 @generator.command()
-@click.argument('repopath')
+@click.argument('repopath', default='.')
 @click.option("--types", default='feat,fix,refactor,docs', help="commit types to show in changelog")
 @click.option("--bodytags", default='BREAKING CHANGE,MAJOR', help="body tags that schould be shown in changelog")
 def generate(repopath, types, bodytags):
@@ -23,23 +25,27 @@ def generate(repopath, types, bodytags):
         print('Done!')
 
 @generator.command()
-@click.argument('repopath')
+@click.argument('repopath', default='.')
 @click.option("--types", default='feat,fix,refactor,docs', help="commit types to show in changelog")
 @click.option("--bodytags", default='BREAKING CHANGE,MAJOR', help="body tags that schould be shown in changelog")
 def add(repopath, types, bodytags):
     repo = Repo(repopath)
     types = types.split(',')
     bodytags = bodytags.split(',')
-    with open(repopath + '/' + filename, 'r') as in_file:
-        old_text = in_file.read()
+    try:
+        with open(os.path.join(repopath, filename), 'r') as in_file:
+            old_text = in_file.read()
+    except FileNotFoundError:
+        print("No CHANGELOG.md found")
+        return
     text = repo.add_changelog(old_text, types, bodytags)
     if text:
-        with open(repopath + '/' + filename, 'w') as out_file:
+        with open(os.path.join(repopath, filename), 'w') as out_file:
             out_file.write(text)
         print('Done!')
 
 @generator.command()
-@click.argument('repopath')
+@click.argument('repopath', default='.')
 @click.option("--types", default='feat,fix,refactor,docs', help="commit types to show in changelog")
 @click.option("--bodytags", default='BREAKING CHANGE,MAJOR', help="body tags that schould be shown in changelog")
 def printout(repopath, types, bodytags):
